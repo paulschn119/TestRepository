b9e7dbc006c5c3c34f38961057f98a910ca8bb46
paulschn119 <paul.schneider@student.hnu.de>
2024-11-13 10:39:32 +0100

    neue Ausgabe

--- Testclass.java
+++ Testclass.java
@@ -3,6 +3,7 @@ public class Testclass{
     // Kommentar
     public static void main (String[] args){
         System.out.println("Hello zusammen");
+        System.out.println("CR7 > Messi");
     }
 
 }
